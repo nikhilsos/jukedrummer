dd7199abd91f4f1db569b80a6612aad07c56e12e
Nikhil Thapa <nikhiltesla@gmail.com>
2025-10-29 08:54:21 +0000

    modify e_v_r_m.py to normalize the specs and use get_vqvae function

--- extract_vqvae_recon_mel.py
+++ extract_vqvae_recon_mel.py
@@ -5,6 +5,9 @@ import tqdm
 from model.vqvae import VQVAE, Sampler
 from train_vqvae import setup_model
 from hparams import setup_vq_hparams
+from utils.functions import get_vqvae
+
+
 # from hifi_gan.meldataset import spectral_normalize_torch
 
 import soundfile as sf
@@ -22,14 +25,22 @@ hps = setup_vq_hparams(f'vq{VQ_IDX}')
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 vqvae = setup_model(hps, device)
 
+#TODO : Use get vqvae function and normalise the reconstructed mel saved so that the normalization during vocoder can be omitted.
+target_vqvae, _, target_mean, target_std = get_vqvae(
+        vq_idx=VQ_IDX,
+        data_type='target', 
+        ckpt_dir=hps.ckpt_dir, 
+        device = device
+    )
+
 # Load checkpoint
-ckpt = torch.load(CHECKPOINT_PATH, map_location=device, weights_only=False)
-if 'model' in ckpt:
-    vqvae.load_state_dict(ckpt['model'])
-elif 'state_dict' in ckpt:
-    vqvae.load_state_dict(ckpt['state_dict'])
-else:
-    vqvae.load_state_dict(ckpt)
+# ckpt = torch.load(CHECKPOINT_PATH, map_location=device, weights_only=False)
+# if 'model' in ckpt:
+#     vqvae.load_state_dict(ckpt['model'])
+# elif 'state_dict' in ckpt:
+#     vqvae.load_state_dict(ckpt['state_dict'])
+# else:
+#     vqvae.load_state_dict(ckpt)
 
 # Loop over token files and decode to mel
 for fname in tqdm.tqdm(os.listdir(TOKEN_DIR), desc="Decoding tokens"):
@@ -46,7 +57,7 @@ for fname in tqdm.tqdm(os.listdir(TOKEN_DIR), desc="Decoding tokens"):
     tokens = torch.from_numpy(tokens).long().to(device)
 
     with torch.no_grad():
-        recon_mel = vqvae.decode(tokens).squeeze(0).detach().cpu().numpy()  # [1, 80, T]
+        recon_mel = target_vqvae.decode(tokens).squeeze(0).detach().cpu().numpy()  # [1, 80, T]
         # recon_mel = recon_mel.squeeze(0) 
          # [80, T]
 
@@ -60,7 +71,7 @@ for fname in tqdm.tqdm(os.listdir(TOKEN_DIR), desc="Decoding tokens"):
     recon_mel_resized = np.zeros_like(original_mel)
     min_len = min(recon_mel.shape[1], original_mel.shape[1])
     recon_mel_resized[:, :min_len] = recon_mel[:, :min_len]
-    recon_mel = recon_mel_resized
+    recon_mel = recon_mel_resized * (target_std.cpu().numpy()) + target_mean.cpu().numpy()
     out_path = os.path.join(OUTPUT_DIR, fname)
     np.save(out_path, recon_mel)
     # check if recostructed mel shape is same to the original mel
